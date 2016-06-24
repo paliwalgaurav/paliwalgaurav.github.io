7905060c84e8fe70415e3438d6ba4b97a34a7a6b
Gaurav K. Paliwal <paliwalgaurav@users.noreply.github.com>
2016-06-24 16:30:40 +0530

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ var orange = [40, 100, 60];
 var green = [75, 100, 40];
 var blue = [196, 77, 55];
 var purple = [280, 50, 60];
-var letterColors = [red, orange, green, blue, purple];
+var letterColors = [green, orange, red, blue, purple];
 
 drawName(myName, letterColors);
 
